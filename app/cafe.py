70d57709d140dbc6ff0d01752422cc90f9383ed3
Nicholasname <tygyrdyk@users.noreply.github.com>
2025-12-05 12:16:49 +0200

    added solution, tested, refactored to flake8

--- app/cafe.py
+++ app/cafe.py
@@ -4,14 +4,14 @@ import app.errors as errors
 
 
 class Cafe():
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
-        if not visitor["vaccine"]:
-            raise errors.NotVaccinatedError
+        if not visitor.get("vaccine"):
+            raise errors.NotVaccinatedError()
         if datetime.date.today() > visitor["vaccine"]["expiration_date"]:
-            raise errors.OutdatedVaccineError
+            raise errors.OutdatedVaccineError()
         if not visitor["wearing_a_mask"]:
-            raise errors.NotWearingMaskError
+            raise errors.NotWearingMaskError()
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-import datetime
-
 import app.errors as errors
 
 import app.cafe as cafeclass
 
-def go_to_cafe(friends: list, cafe: cafeclass.Cafe) -> str:
+
+def go_to_cafe(
+        friends: list,
+        cafe: cafeclass.Cafe
+) -> str:
     masks_to_buy = 0
     for friend in friends:
         try:
@@ -13,6 +15,6 @@ def go_to_cafe(friends: list, cafe: cafeclass.Cafe) -> str:
             return "All friends should be vaccinated"
         except errors.NotWearingMaskError:
             masks_to_buy += 1
-        if masks_to_buy:
-            return f"Friends should buy {masks_to_buy} masks"
-        return f"Friends can go to {cafe.name}"
+    if masks_to_buy:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
